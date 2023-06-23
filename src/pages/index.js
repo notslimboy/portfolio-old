ed5944932bc979432688f0072839d7365abfdde2
Raka Arya Pratama <raka.arya34@gmail.com>
2023-06-24 00:55:56 +0700

    update

--- src/pages/index.js
+++ src/pages/index.js
@@ -28,7 +28,7 @@ export default function Home() {
       <main className='flex items-center text-dark w-full min-h-screen dark:text-light sm:items-start'>
         <Layout className='pt-0 md:pt-16 sm:pt-16'>
            <div className="flex items-center justify-between w-full lg:flex-col">
-               <div className='w-1/2 md:w-full'>
+               <div className='w-1/2 md:w-full p-5'>
                 <Image src={profilePic} alt="Raka" className='w-full h-auto lg:hidden md:inline-block md:w-full'
                 priority
                 sizes="(max-width: 768px) 100vw,
@@ -36,7 +36,7 @@ export default function Home() {
               50vw"
                  />
                </div>
-               <div className='w-1/2 flex flex-col items-center self-center lg:w-full lg:text-center'>
+               <div className='w-1/2 flex flex-col items-center self-center ml-15 lg:w-full lg:text-center'>
                 <AnimatedText text="A good game designer is always learning." className='!text-6xl !text-left
                 xl:!text-5xl lg:!text-center lg:!text=6xl md:!text-5xl sm:!text-3xl
                 '/>
